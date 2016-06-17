0329573ad1c96c90bf1075a90b2c6fcb06a8107a
Stephanie Sutanto <ssutanto@infusion.com>
2016-06-17 15:12:52 -0400

    Update UI and pagination

--- public/js/controllers/ProjectCtrl.js
+++ public/js/controllers/ProjectCtrl.js
@@ -1,19 +1,41 @@
 angular.module('ProjectCtrl', []).controller('ProjectController',['$scope', 'Project','$location', function($scope, Project, $location) {
-
+	$scope.model = {};
+    $scope.pageOptions = [{value:"5"},{value:"10"},{value:"25"}, {value: "All"}];
 	$scope.headings = ["Status","Project Name", "AM", "Baseline Hours", "Budget", "Client", "Currency"];
-	$scope.pageOptions = [5,10,25, 50];
 	$scope.projects = [];
 	$scope.currentPage = 0;
     $scope.pageSize = 10;
+	$scope.totalPages = 0;
+	$scope.hidePagination = false;
+	$scope.numberOfPages = function(){
+		return $scope.totalPages / $scope.pageSize;
+	} 
 
 	Project.get().then(function(response) {
         $scope.projects = response.data;
-        $scope.numberOfPages = response.data.length;
+        $scope.totalPages = response.data.length;
+        $scope.numberOfPages();
     }, function(error) {
         console.log('Error:' + err);
     });
 
-    $scope.go = function ( path ) {
+    $scope.go = function (path) 
+    {
 		$location.path( path );
     };
+
+    $scope.updatePageSettings = function ()
+    {
+    	$scope.currentPage = 0;
+    	if ($scope.selectedPageSize.value === "All")
+    	{
+    		$scope.pageSize = $scope.totalPages;
+    		$scope.hidePagination = true;
+    	}
+    	else {
+    		$scope.pageSize = parseInt($scope.selectedPageSize.value);
+    		$scope.hidePagination = false;
+    	}
+    	
+    }
 }]);
